91c851ad1b90cd66e6715579d7479bf342082dc6
Cicak Kayang <eriscapricron@gmail.com>
2022-07-03 20:19:46 +0700

    done

--- controllers/gtrendsController.js
+++ controllers/gtrendsController.js
@@ -15,6 +15,16 @@ getGtrends = function(req, res) {
         res.json({
             value: value
         })
+
+    //     let xml = `<?xml version="1.0" encoding="UTF-8"?>
+    //     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    //         <data>
+    //             <value>${value}</value>
+    //         </data>
+    //     </urlset>`
+
+    // res.header("Content-Type", "application/xml");
+    // res.status(200).send(xml);
     })
     .catch(function(err){
       console.error('Oh no there was an error', err);
